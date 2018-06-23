ab483a639ecc220c275b4c899748718aa8afef8d
linclsdy <40482603+linclsdy@users.noreply.github.com>
2018-06-23 23:21:52 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# memory-game
-This is a memory game.
+# Memory Game Project
+
+
+## How to play
+
+* Please go to https://github.com/linclsdy/memory-game This is a memory game with 8 pairs of same cards.
+* Click to turn two cards open and it counts one move. 
+* If these two cards are matched, they are opened on the deck.
+* If they are not matched, both will be turned back.
+* Try to remember each card's position.
+* Try to match all pairs of cards in as minimum moves and times.
+
+
+### How to score
+
+* You will get 3 stars of 3 star rating by less than 20 moves.
+* If your moves are over 20, it will deducts one star. 
+* If your moves are over 30, it will deducts two stars.
+
+
+Good luck! 
---
+++ js/app.js
@@ -0,0 +1,205 @@
+/*
+ * Create a list that holds all of your cards
+ */
+
+const cards = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-leaf", "fa fa-bicycle", "fa fa-bomb", 
+"fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-leaf", "fa fa-bicycle", "fa fa-bomb"];
+
+const deck = document.querySelector('.deck');
+
+let toggled = [];
+let moves = 0;
+let stars = 3; 
+let removeStar = 0;
+let clockOff = true;
+let time = 0;
+let matches = 0;
+
+//shuffle the cards on the deck
+function shuffleDeck() {
+  //const shuffleCards = Array.from(document.querySelectorAll('.deck li'));
+  //console.log('shuffle cards', shuffleCards);
+  const shuffledCards = shuffle(cards);
+  for (shuffledCard of shuffledCards) {
+    var newShuffledCard = document.createElement('li');
+    newShuffledCard.className = 'card';
+    newShuffledCard.innerHTML = '<i class="'+ shuffledCard + '"></i>';
+    deck.appendChild(newShuffledCard);
+  }
+}
+shuffleDeck();
+
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+
+
+//while toggle two cards, it will check match, check star and add one move.
+deck.addEventListener('click', event => {
+  const clickTarget = event.target;
+  if (clickTarget.classList.contains('card') && toggled.length < 2 && !toggled.includes(clickTarget)) {
+       toggleCard(clickTarget);
+       addToggleCard(clickTarget);
+       if (toggled.length === 2) {
+      // console.log ("2 cards!");
+      // console.log (toggled);
+       checkMatch(clickTarget);
+       addMove();
+       checkStar();
+       }
+          if (clockOff) {
+            startClock();
+            clockOff = false;
+          }
+      }
+  
+//end game after all cards are matched. It pops up an alert. 
+  if(checkFinish()) {
+    if(confirm("Congratulation!! You have used " + moves + " moves in " + time + " seconds with " + stars + " out of 3 stars rating. Do you want to play again?")) {
+     restart();
+      };
+  }
+  }); 
+
+
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+//restart the game
+function restart() {
+    location.reload();
+    console.log(checkStar);
+    startClock();
+
+}
+
+//flip to open the card and show the card
+function toggleCard(clickTarget) {
+    clickTarget.classList.toggle('open');
+    clickTarget.classList.toggle('show');
+}
+
+//record toggled cards 
+function addToggleCard(clickTarget) {
+    toggled.push(clickTarget);
+    //console.log(toggled);
+}
+
+//after open the card, it checks whether the cards are matched. If not, it will close the card.
+function checkMatch() {
+    if (
+    toggled[0].firstElementChild.className === toggled[1].firstElementChild.className
+    ) {
+          toggled[0].classList.toggle('match');
+          toggled[1].classList.toggle('match');
+          toggled = [];
+          matches++;
+          //console.log('Match!');
+    } else {
+           //console.log("Not match!");
+           setTimeout( () => {
+           toggleCard(toggled[0]);
+           toggleCard(toggled[1]);
+           toggled = []; 
+           }, 800);
+  }
+}
+
+//check whether 8 pairs of cards are matched
+function checkFinish() {
+  console.log(matches);
+  if (matches === 8) {
+    return true; 
+  } else {
+    return false;
+  }
+}
+
+//while click on 2 cards, it adds one move
+function addMove() {
+  moves++ ;
+  const movesText = document.querySelector('.moves');
+  movesText.innerText = moves;
+  //console.log(moves);
+}
+
+//if moves are more then 25, it displays 2 stars; if moves are more than 35, it displays only 1 star. 
+function checkStar(){
+  if (moves === 20 || moves === 30) {
+    stars --;
+    hideStar();
+    //console.log(stars);
+  }
+}
+
+//hide the star while running the ckeckStar function
+function hideStar() {
+  const starList = document.querySelectorAll('.stars li');
+  for (star of starList) {
+    if (star.style.display !== 'none') {
+    star.style.display = 'none';
+    break;
+    }
+  }
+}
+
+//start the timer after first click    
+function startClock() {
+ // time = 0;
+  let clockId = setInterval(() => {
+    time++;
+    displayTime()
+  }, 1000);
+}
+
+//display the timer by minutes and seconds
+function displayTime() {
+  const clock = document.querySelector('.clock');
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  var displayseconds = "0";
+  var displayminutes = "0";
+  if (seconds < 10) {
+    displayseconds = "0" + seconds;
+  }
+  else {
+    displayseconds = seconds;
+  }
+  if (minutes < 10) {
+    displayminutes = "0" + minutes;
+  }
+  else {
+    displayminutes = minutes;
+  }
+  clock.innerHTML = displayminutes + ":" + displayseconds;
+}   
+
+
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
